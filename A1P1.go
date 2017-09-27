df2539a23fb406ed90b5c9a9dddd19707f6d9bac
Sascha da Silva <snd809@mail.usask.ca>
2017-09-27 15:28:04 -0600

    Added searchPlanets function, as well as some structs and changes to creation.

--- A1P1.go
+++ A1P1.go
@@ -9,35 +9,61 @@ type Edge struct {
 	endPlanet string
 }
 
+type Planet struct {
+	name string
+	cost int
+	parent Planet
+	numTransits int
+}
+
+type Tree struct{
+	root Planet
+	curCost int
+}
 
 /* Darth Vader's least expensive blockade program */
 
-func main() {
-	/*Tests begin */
+func readValues() bool{
 
 	/* Can we read values from user? */
 	var numPlanets, numConnections int
 
 	fmt.Println("Give me an int for numPlanets please")
 	fmt.Scan(&numPlanets)
-	fmt.Println("you selected ", numPlanets)
 	/* array of planets */
-	var slicePlanets = make([]string,numPlanets)
+	var slicePlanets = make([]Planet,numPlanets)
 	for i := 0; i < numPlanets; i++ {
-		fmt.Scan(&slicePlanets[i])
+		fmt.Scan(&slicePlanets.name[i])
+		fmt.Scan(&slicePlanets.cost[i])
 	}
 
 	fmt.Println("Give me an int for numConnections please")
 	fmt.Scan(&numConnections)
 	/* array of edges */
 	var sliceTransits = make([]Edge,numConnections)
-	fmt.Println("you selected ", numConnections)
 	for j := 0; j < numConnections; j++ {
 		fmt.Scan(&sliceTransits[j].startPlanet)
 		fmt.Scan(&sliceTransits[j].endPlanet)
 	}
+}
+
+func main() {
+	var sucessRead
+	var successFind Planet
+	var targetPlanet Planet
+	/*Tests begin */
+	successRead = readValues()
 
 	fmt.Println(slicePlanets)
 	fmt.Println(sliceTransits)
+
+	success2 = searchTransits()
+
+
+	/* Now trying to search and find the shortest path */
+	if successFind = nil {
+	fmt.Println("Darth Blockades ", targetPlanet.name)
+	} else{
+	fmt.Println("Leia escapes with the plans!")
 }
 
