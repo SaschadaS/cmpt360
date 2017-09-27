acc72b11068c811fd292d896686b05c7f62638e9
Sascha da Silva <snd809@mail.usask.ca>
2017-09-27 14:02:44 -0600

    Added struct for edges and an attempt at reading values.

--- A1P1.go
+++ A1P1.go
@@ -4,6 +4,12 @@ import (
 	"fmt"
 )
 
+type Edge struct {
+	startPlanet string
+	endPlanet string
+}
+
+
 /* Darth Vader's least expensive blockade program */
 
 func main() {
@@ -11,18 +17,24 @@ func main() {
 
 	/* Can we read values from user? */
 	var numPlanets, numConnections int
-	var edges string
+	var transit1 Edge
 
-	fmt.Println("Give me an int for numPlanets please\n")
+	fmt.Println("Give me an int for numPlanets please")
 	fmt.Scan(&numPlanets)
 	fmt.Println("you selected ", numPlanets)
-	fmt.Println("Give me an int for numConnections please\n")
+	/* array of planets */
+	var planets [numPlanets]string
+	fmt.Println("Give me an int for numConnections please")
 	fmt.Scan(&numConnections)
+	/* array of edges */
+	var transits [numConnections]Edge
 	fmt.Println("you selected ", numConnections)
 
-	fmt.Println("Give me a string to read please\n")
-	fmt.Scan(&edges)
-	fmt.Println("You gave me ", edges)
+	fmt.Println("Give me a string to read please")
+	fmt.Scan(&transit1.startPlanet)
+	fmt.Scan(&transit1.endPlanet)
+	transits[0] = transit1
+	fmt.Println("You gave me ", transit1)
 
 }
 
