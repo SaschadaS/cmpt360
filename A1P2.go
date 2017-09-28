7628f21531705b60ea09d578ad68adf301a24a4a
Sascha da Silva <snd809@mail.usask.ca>
2017-09-27 21:06:32 -0600

    Fixed some input, still fixing for tests.

--- A1P2.go
+++ A1P2.go
@@ -25,24 +25,24 @@ type query struct{
 	endTime int
 }
 
-func readTrace(numTrace int) []trace {
+func readTrace(numTrace int) *[]trace {
 	var TraceData []trace
 	for i := 0; i<numTrace; i++ {
-	fmt.Scan(TraceData[i].source, TraceData[i].destination, TraceData[i].time)
+	fmt.Scan(&TraceData[i].source, &TraceData[i].destination, &TraceData[i].time)
 	}
-	return TraceData
+	return &TraceData
 }
 
-func readQuery(numQueries int) []query {
+func readQuery(numQueries int) *[]query {
 	var QueryData []query
 	for i := 0; i<numQueries; i++ {
-	fmt.Scan(QueryData[i].startComp, QueryData[i].startTime, QueryData[i].endComp, QueryData[i].endTime)
+	fmt.Scan(&QueryData[i].startComp, &QueryData[i].startTime, &QueryData[i].endComp, &QueryData[i].endTime)
 	}
-	return QueryData
+	return &QueryData
 }
 
-func checkQueries(queryData []query, traceData []trace, isInfected []bool){
-for _, y := range queryData {
+func checkQueries(queryData *[]query, traceData *[]trace, isInfected []bool){
+for _, y := range *queryData {
 
 	if isInfected[y.endComp+1] == true{
 	fmt.Println("Y")
@@ -55,13 +55,20 @@ for _, y := range queryData {
 func main(){
 
 var numComputers, numTrace, numQueries int
-fmt.Scan(numComputers, numTrace)
-var isInfected []bool
-var traceData []trace =readTrace(numTrace)
+fmt.Scanln(&numComputers)
+fmt.Scanln(&numTrace)
+//var isInfected []bool
+traceData := make([]trace, numTrace)
+//var queryData []query
+fmt.Scan(&traceData[0].source)
+fmt.Scan(&traceData[0].destination)
+fmt.Scan(&traceData[0].time)
+fmt.Println(traceData[0])
+//var traceData *[]trace =readTrace(numTrace)
 fmt.Scan(numQueries)
-var queryData []query =readQuery(numQueries)
+//var queryData *[]query =readQuery(numQueries)
 
-checkQueries(queryData, traceData, isInfected)
+//checkQueries(queryData, traceData, isInfected)
 
 fmt.Println("Compiled successfully!")
 
