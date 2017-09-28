c1e1417dc3095a88e4c2ab1de6c2759b879a0501
Sascha da Silva <snd809@mail.usask.ca>
2017-09-27 20:41:20 -0600

    Had to elaborate functions to make tests logical. Base cases now compile.

--- A1P2.go
+++ A1P2.go
@@ -8,6 +8,7 @@ Assignment 1 Programming question 2
 
 Identifying virus spread in a list of computer interactions.
  */
+package main
 
 import "fmt"
 
@@ -24,15 +25,43 @@ type query struct{
 	endTime int
 }
 
+func readTrace(numTrace int) []trace {
+	var TraceData []trace
+	for i := 0; i<numTrace; i++ {
+	fmt.Scan(TraceData[i].source, TraceData[i].destination, TraceData[i].time)
+	}
+	return TraceData
+}
+
+func readQuery(numQueries int) []query {
+	var QueryData []query
+	for i := 0; i<numQueries; i++ {
+	fmt.Scan(QueryData[i].startComp, QueryData[i].startTime, QueryData[i].endComp, QueryData[i].endTime)
+	}
+	return QueryData
+}
+
+func checkQueries(queryData []query, traceData []trace, isInfected []bool){
+for _, y := range queryData {
+
+	if isInfected[y.endComp+1] == true{
+	fmt.Println("Y")
+	}else {
+	fmt.Println("N")
+	}
+}
+}
+
 func main(){
 
-var isInfected []bool
 var numComputers, numTrace, numQueries int
+fmt.Scan(numComputers, numTrace)
+var isInfected []bool
+var traceData []trace =readTrace(numTrace)
+fmt.Scan(numQueries)
+var queryData []query =readQuery(numQueries)
 
-var traceData []trace =readTrace()
-var queryData []query =readQuery()
-
-checkQueries(comp1 int, trace1 int, comp2 int, trace2 int)
+checkQueries(queryData, traceData, isInfected)
 
 fmt.Println("Compiled successfully!")
 
